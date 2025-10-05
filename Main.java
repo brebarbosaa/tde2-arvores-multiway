5f6f8b2d5dddd27a1da4b2530032fd0a0d1ba340
Alana Queiroz <166722191+alanaqroz@users.noreply.github.com>
2025-10-05 13:07:13 -0300

    Update Main.java
    exemplos remoção

--- Main.java
+++ Main.java
@@ -7,8 +7,25 @@ public class Main{
         in.inserir(raiz, "arvore");
         in.inserir("galho");
         in.inserir("gato");
+        trie.inserir("galo");
+        trie.inserir("g");
+
+        
 
         System.out.println(ArvoreTrie.busca("arvore")); //palavra encontrada
         System.out.println(ArvoreTrie.busca("arvor")); //palavra não encontrada
+        System.out.println("Busca por 'galho': " + trie.busca("galho")); 
+        System.out.println("Busca por 'g': " + trie.busca("g"));
+
+        trie.remover("gato");
+        trie.remover("galho");
+        trie.remover("g");
+
+                // Buscas depois da remoção
+        System.out.println("\n--- Após remoções ---");
+        System.out.println("Busca por 'gato': " + trie.busca("gato"));       // false
+        System.out.println("Busca por 'galho': " + trie.busca("galho"));     // false
+        System.out.println("Busca por 'galo': " + trie.busca("galo"));       // true
+        System.out.println("Busca por 'g': " + trie.busca("g"));             // false
     }
 }
