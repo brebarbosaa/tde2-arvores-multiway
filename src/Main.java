39ce0a59d6416b65125c6a36ea846ed011622e95
Brenda Barbosa <151410709+brebarbosaa@users.noreply.github.com>
2025-10-05 23:00:13 -0300

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        ArvoreTrie raiz = new ArvoreTrie();
-
-        Inserir in = new Inserir();
-
-        in.inserir(raiz, "arvore");
-    }
-}
