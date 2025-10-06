989c486bd9e33a3b5cdb5343d56e63a67fc67427
Brenda Barbosa <151410709+brebarbosaa@users.noreply.github.com>
2025-10-05 22:58:03 -0300

    atualização do main

--- Main.java
+++ Main.java
@@ -1,31 +1,26 @@
-public class Main{
-    public static void main(String[] args){
-        ArvoreTrie raiz = new ArvoreTrie();
+public class Main {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
 
-        inserir in = new inserir();
-
-        in.inserir(raiz, "arvore");
-        in.inserir("galho");
-        in.inserir("gato");
+        trie.inserir("arvore");
+        trie.inserir("galho");
+        trie.inserir("gato");
         trie.inserir("galo");
         trie.inserir("g");
 
-        
-
-        System.out.println(ArvoreTrie.busca("arvore")); //palavra encontrada
-        System.out.println(ArvoreTrie.busca("arvor")); //palavra não encontrada
-        System.out.println("Busca por 'galho': " + trie.busca("galho")); 
+        System.out.println(trie.busca("arvore")); // true
+        System.out.println(trie.busca("arvor"));  // false
+        System.out.println("Busca por 'galho': " + trie.busca("galho"));
         System.out.println("Busca por 'g': " + trie.busca("g"));
 
         trie.remover("gato");
         trie.remover("galho");
         trie.remover("g");
 
-                // Buscas depois da remoção
         System.out.println("\n--- Após remoções ---");
-        System.out.println("Busca por 'gato': " + trie.busca("gato"));       // false
-        System.out.println("Busca por 'galho': " + trie.busca("galho"));     // false
-        System.out.println("Busca por 'galo': " + trie.busca("galo"));       // true
-        System.out.println("Busca por 'g': " + trie.busca("g"));             // false
+        System.out.println("Busca por 'gato': " + trie.busca("gato"));   // false
+        System.out.println("Busca por 'galho': " + trie.busca("galho")); // false
+        System.out.println("Busca por 'galo': " + trie.busca("galo"));   // true
+        System.out.println("Busca por 'g': " + trie.busca("g"));         // false
     }
 }
